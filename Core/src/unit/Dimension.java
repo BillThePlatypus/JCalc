1e9cff82d9616515ab5e0c35e205fb8d85f38bc6
Trey Henrichsen <treyhenrichsen@gmail.com>
2018-02-02 09:40:27 -0700

    Move static methods from Dimension to Dimensions

--- Core/src/unit/Dimension.java
+++ Core/src/unit/Dimension.java
@@ -8,44 +8,7 @@ import java.util.TreeSet;
 
 public abstract class Dimension implements Comparable<Dimension>
 {
-	private static Dimension makeDimension(TreeMap<BaseDimension, Integer> dims)
-	{
 
-		Set<Map.Entry<BaseDimension, Integer>> entries = dims.entrySet();
-		if (entries.size() == 1)
-			for (Map.Entry<BaseDimension, Integer> entry : entries)
-				if (entry.getValue() == 1)
-					return entry.getKey();
-				else if (entries.size() == 0)
-					return new NullDimension();
-		return new CompoundDimension(dims);
-	}
-
-	public static Dimension multiply(Dimension d1, Dimension d2)
-	{
-
-		TreeMap<BaseDimension, Integer> dimensions = new TreeMap<>();
-		for (Map.Entry<BaseDimension, Integer> pair : d1.getTypes().entrySet())
-			dimensions.put(pair.getKey(), pair.getValue());
-		for (Map.Entry<BaseDimension, Integer> pair : d2.getTypes().entrySet())
-			if (dimensions.containsKey(pair.getKey()))
-				dimensions.put(pair.getKey(), dimensions.get(pair.getKey()) + pair.getValue());
-			else
-				dimensions.put(pair.getKey(), pair.getValue());
-		Set<BaseDimension> keys = new TreeSet<>(dimensions.keySet());
-		for (BaseDimension dim : keys)
-			if (dimensions.get(dim) == 0)
-				dimensions.remove(dim);
-		return makeDimension(dimensions);
-	}
-
-	public static Dimension divide(Dimension dim1, Dimension dim2)
-	{
-		System.out.println(dim1);
-		System.out.println(dim2);
-		System.out.println(dim2.invert());
-		return Dimension.multiply(dim1, dim2.invert());
-	}
 
 	@Override
 	public String toString()
@@ -79,7 +42,7 @@ public abstract class Dimension implements Comparable<Dimension>
 		TreeMap<BaseDimension, Integer> dims = this.getTypes();
 		for (BaseDimension dim : dims.keySet())
 			dims.put(dim, -dims.get(dim));
-		return makeDimension(dims);
+		return Dimensions.makeDimension(dims);
 	}
 
 
--- Core/src/unit/Dimensions.java
+++ Core/src/unit/Dimensions.java
@@ -1,7 +1,9 @@
 package unit;
 
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
+import java.util.TreeSet;
 
 public class Dimensions
 {
@@ -89,5 +91,43 @@ public class Dimensions
 				}
 			}
 		return builder.toString();
+	}
+	protected static Dimension makeDimension(TreeMap<BaseDimension, Integer> dims)
+	{
+
+		Set<Map.Entry<BaseDimension, Integer>> entries = dims.entrySet();
+		if (entries.size() == 1)
+			for (Map.Entry<BaseDimension, Integer> entry : entries)
+				if (entry.getValue() == 1)
+					return entry.getKey();
+				else if (entries.size() == 0)
+					return new NullDimension();
+		return new CompoundDimension(dims);
+	}
+
+	public static Dimension multiply(Dimension d1, Dimension d2)
+	{
+
+		TreeMap<BaseDimension, Integer> dimensions = new TreeMap<>();
+		for (Map.Entry<BaseDimension, Integer> pair : d1.getTypes().entrySet())
+			dimensions.put(pair.getKey(), pair.getValue());
+		for (Map.Entry<BaseDimension, Integer> pair : d2.getTypes().entrySet())
+			if (dimensions.containsKey(pair.getKey()))
+				dimensions.put(pair.getKey(), dimensions.get(pair.getKey()) + pair.getValue());
+			else
+				dimensions.put(pair.getKey(), pair.getValue());
+		Set<BaseDimension> keys = new TreeSet<>(dimensions.keySet());
+		for (BaseDimension dim : keys)
+			if (dimensions.get(dim) == 0)
+				dimensions.remove(dim);
+		return makeDimension(dimensions);
+	}
+
+	public static Dimension divide(Dimension dim1, Dimension dim2)
+	{
+		System.out.println(dim1);
+		System.out.println(dim2);
+		System.out.println(dim2.invert());
+		return Dimensions.multiply(dim1, dim2.invert());
 	}
 }
