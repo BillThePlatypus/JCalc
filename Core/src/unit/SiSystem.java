9490c33a5e5c475f3b196b933493b6288b519797
Trey Henrichsen <treyhenrichsen@gmail.com>
2018-02-02 11:39:03 -0700

    Added more units to SiSystem

--- Core/src/unit/SiSystem.java
+++ Core/src/unit/SiSystem.java
@@ -1,13 +1,44 @@
 package unit;
 
 
+import java.util.TreeMap;
+
 public class SiSystem extends UnitSystem
 {
-	public static BaseUnit KILOGRAM=new BaseUnit(Dimensions.MASS,1,"kilogram","kg");
-	public static BaseUnit GRAM=new BaseUnit(Dimensions.MASS,.001,"gram","g");
-	public static BaseUnit METER=new BaseUnit(Dimensions.LENGTH,1,"meter","m");
-	public static BaseUnit SECOND=new BaseUnit(Dimensions.TIME,1,"second","s");
-	public static BaseUnit AMPERE=new BaseUnit(Dimensions.CURRENT,1,"Amp","A");
+	public static final BaseUnit KILOGRAM=new BaseUnit(Dimensions.MASS,1,"kilogram","kg");
+	public static final BaseUnit GRAM=new BaseUnit(Dimensions.MASS,.001,"gram","g");
+	public static final BaseUnit METER=new BaseUnit(Dimensions.LENGTH,1,"meter","m");
+	public static final BaseUnit SECOND=new BaseUnit(Dimensions.TIME,1,"second","s");
+	public static final BaseUnit AMPERE=new BaseUnit(Dimensions.CURRENT,1,"Amp","A");
+	public static final CompoundUnit COULOMB;
+	public static final CompoundUnit JOULE;
+	public static final CompoundUnit WATT;
+	public static final CompoundUnit DIOPTER;
+	public static final CompoundUnit HERTZ;
+	public static final CompoundUnit NEWTON;
+
+	static{
+		TreeMap<Unit,Integer> tempMap=new TreeMap<>();
+		tempMap.put(SECOND,-1);
+		HERTZ=new CompoundUnit(tempMap,"HERTZ","hz");
+		tempMap.remove(SECOND);
+		tempMap.put(METER,-1);
+		DIOPTER=new CompoundUnit(tempMap);
+		tempMap.put(METER,1);
+		tempMap.put(KILOGRAM,1);
+		tempMap.put(SECOND,-2);
+		NEWTON=new CompoundUnit(tempMap);
+		tempMap.put(METER,2);
+		JOULE=new CompoundUnit(tempMap);
+		tempMap.put(SECOND,-3);
+		WATT=new CompoundUnit(tempMap);
+		tempMap.clear();
+		tempMap.put(SECOND,1);
+		tempMap.put(AMPERE,1);
+		COULOMB =new CompoundUnit(tempMap);
+	}
+
+
 	@Override
 	public String getName()
 	{
