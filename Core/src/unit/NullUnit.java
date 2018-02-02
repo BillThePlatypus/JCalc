9d32912a897b3f594996b968f79fa6ce5dd0aa13
Trey Henrichsen <treyhenrichsen@gmail.com>
2018-02-02 11:40:06 -0700

    Created a NullUnit class

---
+++ Core/src/unit/NullUnit.java
@@ -0,0 +1,47 @@
+package unit;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class NullUnit extends Unit{
+
+	public static final NullUnit INSTANCE=new NullUnit();
+
+	private NullUnit(){}
+	@Override
+	public String getSymbol() {
+		return "";
+	}
+
+	@Override
+	public String getFullName() {
+		return "";
+	}
+
+	@Override
+	public Dimension getDimension() {
+		return NullDimension.INSTANCE;
+	}
+
+	@Override
+	public List<UnitSystem> getPreferredSystems() {
+		return new ArrayList<>();
+	}
+
+	@Override
+	public double asSi() {
+		return 0;
+	}
+
+	@Override
+	public void addSystem(UnitSystem system) {
+
+	}
+
+	@Override
+	public Map<BaseUnit, Integer> asBaseUnits() {
+		return new TreeMap<>();
+	}
+}
