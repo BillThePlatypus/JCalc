4486d596b96eb727f55ce5d7607135442ebbf2e6
Trey Henrichsen <treyhenrichsen@gmail.com>
2018-02-02 09:57:18 -0700

    Make static Units class for common methods between units

---
+++ Core/src/unit/Units.java
@@ -0,0 +1,25 @@
+package unit;
+
+import java.util.Map;
+import java.util.Set;
+
+public class Units {
+	public static Unit multiply(Unit u1, Unit u2)
+	{
+
+	}
+	public static Unit divide(Unit u1, Unit u2)
+	{
+
+	}
+	public static Unit invert(Unit u)
+	{
+
+	}
+	protected static Unit makeUnit(Map<Unit,Integer> units)
+	{
+		Set<Map.Entry<Unit,Integer>> entries=units.entrySet();
+		if(entries.size()==1)
+
+	}
+}
