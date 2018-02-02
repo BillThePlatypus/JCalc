206ea50d0c69c11d5e71483864edd295e29395bf
Trey Henrichsen <treyhenrichsen@gmail.com>
2018-02-02 09:57:50 -0700

    Add some units to the SiSystem class

--- Core/src/unit/SiSystem.java
+++ Core/src/unit/SiSystem.java
@@ -3,6 +3,11 @@ package unit;
 
 public class SiSystem extends UnitSystem
 {
+	public static BaseUnit KILOGRAM=new BaseUnit(Dimensions.MASS,1,"kilogram","kg");
+	public static BaseUnit GRAM=new BaseUnit(Dimensions.MASS,.001,"gram","g");
+	public static BaseUnit METER=new BaseUnit(Dimensions.LENGTH,1,"meter","m");
+	public static BaseUnit SECOND=new BaseUnit(Dimensions.TIME,1,"second","s");
+	public static BaseUnit AMPERE=new BaseUnit(Dimensions.CURRENT,1,"Amp","A");
 	@Override
 	public String getName()
 	{
